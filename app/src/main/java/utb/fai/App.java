5a2606e7cdec8ff699da4d0a69cd341bea4b382c
Marek Matulík <m_matulik@utb.cz>
2024-09-26 08:56:39 +0200

    email sender module

--- app/src/main/java/utb/fai/App.java
+++ app/src/main/java/utb/fai/App.java
@@ -4,10 +4,23 @@ public class App {
 
     public static void main(String[] args) {
         // TODO: Implement input parameter processing
+
+        if (args.length < 6) {
+            System.out.println("Málo argumentů");
+            System.out.println("java -jar app.jar <smtpServer> <port> <fromEmail> <toEmail> <subject> <body>");
+            return;
+        }
+
+        String smtpServer = args[0];
+        int port = Integer.parseInt(args[1]);
+        String fromEmail = args[2];
+        String toEmail = args[3];
+        String subject = args[4];
+        String body = args[5];
         
         try {
-            EmailSender sender = new EmailSender("smtp.utb.cz", 25);
-            sender.send("you@utb.cz", "you@utb.cz", "Email from Java", "Funguje to?\nSnad...");
+            EmailSender sender = new EmailSender(smtpServer, port);
+            sender.send(fromEmail, toEmail, subject, body);
             sender.close();
         } catch (Exception e) {
             e.printStackTrace();
--- app/src/main/java/utb/fai/EmailSender.java
+++ app/src/main/java/utb/fai/EmailSender.java
@@ -4,6 +4,12 @@ import java.net.*;
 import java.io.*;
 
 public class EmailSender {
+
+    private Socket socket;
+    private PrintWriter output;
+    private BufferedReader input;
+
+
     /*
      * Constructor opens Socket to host/port. If the Socket throws an exception
      * during opening,
@@ -11,6 +17,11 @@ public class EmailSender {
      */
     public EmailSender(String host, int port) throws UnknownHostException, IOException {
 
+        socket = new Socket(host, port);
+        output = new PrintWriter(socket.getOutputStream(), true);
+        input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        System.out.println(input.readLine());
     }
 
     /*
@@ -20,13 +31,39 @@ public class EmailSender {
      * handled by this method.
      */
     public void send(String from, String to, String subject, String text) throws IOException {
+        // Mail EHLO
+        output.println("EHLO " + from);
+        System.out.println(input.readLine());
 
+        // Mail FROM
+        output.println("FROM:<" + from + ">");
+        System.out.println(input.readLine());
+
+        //Mail TO
+        output.println("TO:<" + to + ">");
+        System.out.println(input.readLine());
+
+        // Data
+        output.println("DATA");
+        System.out.println(input.readLine());
+
+        // Subject
+        output.println("SUBJECT:<" + subject + ">");
+        output.println();
+        output.println(text);
+        output.println("."); // mail must end with dot
+        System.out.println(input.readLine());
     }
 
     /*
      * Sends QUIT and closes the socket
      */
     public void close() {
-
+        output.println("QUIT");
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
